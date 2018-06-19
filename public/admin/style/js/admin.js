f9f9d8c53b8fc96687662e6d0050143f07ae3a51
Blog <2273465837@qq.com>
2018-06-19 17:27:38 +0800

    Contains the entry file index.php and the front-end resource files (images, JavaScript, CSS, etc.)
    包含了入口文件 index.php 和前端资源文件（图片、JavaScript、CSS等）

---
+++ public/admin/style/js/admin.js
@@ -0,0 +1,12 @@
+/*
+* @Author: tangzhifu <2273465837@qq.com>
+* @Date:   2018-06-16 13:11:36
+* @Last Modified by:   tangzhifu
+* @Last Modified time: 2018-06-16 19:21:15
+*/
+
+//刷新当前页面
+function load() {
+    location.reload();
+}
+
