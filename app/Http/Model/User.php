162a9a218798f4dcdafa96730d183ceaeb960819
tangzhifu <tangzhifu@ruiec.cn>
2018-07-11 20:24:18 +0800

    model directory
    Model 模型目录

--- app/Http/Model/User.php
+++ app/Http/Model/User.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class User extends Model
 {
     //关联表
-    protected $table='user';
+    protected $table='admin';
     //主键
     protected $primaryKey='user_id';
     //关闭创建更新时间
