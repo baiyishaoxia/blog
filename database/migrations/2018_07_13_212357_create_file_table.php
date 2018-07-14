ceb80f3f4d7d8a3efaf56066133736a4efd6ef38
tangzhifu <tangzhifu@ruiec.cn>
2018-07-14 19:57:17 +0800

    migration
    数据迁移

---
+++ database/migrations/2018_07_13_212357_create_file_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateFileTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('file', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name',255);
+            $table->string('type',255);
+            $table->boolean('is_enable')->default(null);
+            $table->string('key',255);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('file');
+    }
+}
