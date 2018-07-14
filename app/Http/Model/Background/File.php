deef0957757d0356550dd9d5167718be1c45add3
tangzhifu <tangzhifu@ruiec.cn>
2018-07-14 20:01:25 +0800

    Background Model file
    后台模型 模板类

---
+++ app/Http/Model/Background/File.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Http\Model\Background;
+
+use Illuminate\Database\Eloquent\Model;
+
+class File extends Model
+{
+    /**
+     * 关联到模型的数据表
+     *
+     * @var string
+     */
+    protected $table = 'file';
+    /**
+     * 可以被批量赋值的属性.
+     *
+     * @var array
+     */
+    //protected $fillable = [];
+    /**
+     * 不能被批量赋值的属性,如果你想要让所有属性都是可批量赋值的，可以将 $guarded 属性设置为空数组
+     *
+     * @var array
+     */
+    protected $guarded = [];
+
+    /**
+     * 文件上传的类型
+     * @return array
+     * @author Ruiec.Simba
+     */
+    public static function type(){
+        return[
+            'file'       =>'文件',
+            'image'      =>'图片',
+            'video'      =>'视频'
+        ];
+    }
+
+    /**
+     * 获取正在启用的一个文件服务商
+     * @param $type
+     * @return mixed
+     * @author Ruiec.Simba
+     */
+    public static function enableOne($type){
+        return File::where('is_enable','true')->where('type',$type)->first();
+    }
+}
---
+++ app/Http/Model/Background/FileKey.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Model\Background;
+
+use Illuminate\Database\Eloquent\Model;
+use Carbon\Carbon;
+use Cache;
+
+class FileKey extends Model
+{
+    /**
+     * 关联到模型的数据表
+     *
+     * @var string
+     * @author Ruiec.Simba
+     */
+    protected $table = 'file_key';
+    /**
+     * 不能被批量赋值的属性,如果你想要让所有属性都是可批量赋值的，可以将 $guarded 属性设置为空数组
+     *
+     * @var array
+     * @author Ruiec.Simba
+     */
+    protected $guarded = [];
+
+    /**
+     * 读取KEY
+     * @param $name
+     * @return mixed
+     * @author Ruiec.Simba
+     */
+    public static function read($name){
+        $minutes=Carbon::now()->addMinute(10);
+        $value = Cache::remember('file:'.$name,$minutes, function() use($name){
+            return self::where('name',$name)->value('key');
+        });
+        return $value;
+    }
+}
