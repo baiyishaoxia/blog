fcf8d26ed6f8ab71343fc951e812d5e1f86c42f1
tangzhifu <tangzhifu@ruiec.cn>
2018-07-18 21:49:19 +0800

    Common Model directory
    公共模型文件

--- app/Http/Model/Common/Captcha.php
+++ app/Http/Model/Common/Captcha.php
@@ -6,12 +6,12 @@ use Gregwar\Captcha\CaptchaBuilder;
 
 class Captcha
 {
-    //region 验证码   Panjunwei
+    //region 验证码   tangzhifu
     public static function build($width=100,$height=30,$is_numb=true){
         //生成验证码图片的Builder对象，配置相应属性
         $builder = new CaptchaBuilder();
         if($is_numb){
-            $builder->setPhrase(rand(1000,9999));
+            $builder->setPhrase(rand(100,999));
         }
         //可以设置图片宽高及字体
         $builder->build($width, $height, $font = null);
@@ -19,7 +19,7 @@ class Captcha
         $phrase = $builder->getPhrase();
 
         //把内容存入session
-        \Session::flash('captcha_data', $phrase);
+        \Session::flash('milkcaptcha', $phrase);
         //生成图片
         header("Cache-Control: no-cache, must-revalidate");
         header('Content-Type: image/jpeg');
