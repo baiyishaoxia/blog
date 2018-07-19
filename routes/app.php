3bbc2463ed889c2af61f2ef22eae845fa6a3a8df
tangzhifu <tangzhifu@ruiec.cn>
2018-07-19 20:16:31 +0800

    routes common
    app routes

---
+++ routes/app.php
@@ -0,0 +1,18 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| 接口 Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+Route::group(['middleware'=>['web'],'namespace'=>'Admin'],function () {
+    //接口测试
+    Route::post('users','AppController@postCheckUser');
+    //获取文章
+    Route::get('article','AppController@getArticle');
+});
