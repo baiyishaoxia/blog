c99a16857dd0d629970890c374001b2f3c673ce7
tangzhifu <tangzhifu@ruiec.cn>
2018-07-25 21:02:13 +0800

    routes admin
    admin 路由

--- routes/admin.php
+++ routes/admin.php
@@ -1,13 +1,15 @@
 <?php
-Route::group(['middleware'=>['web','admin.login'],'prefix'=>'admin','namespace'=>'Admin'],function () {
+Route::group(['middleware'=>['web','admin.login'],'namespace'=>'Admin'],function () {
     //region   系统工具        tang
     Route::get('element', 'ReadSystemController@getElement');
     Route::any('syslist', 'ReadSystemController@getList');
     Route::get('systab', 'ReadSystemController@getTab');
     //endregion
 
-    //region   分类列表        tang
-
+    //region   图片裁剪        tang
+    Route::get('img/index', 'SystemToolsController@getIndex');
+    Route::post('img/create', 'SystemToolsController@create');
+    Route::get('img/imgcat', 'SystemToolsController@imgCat');
     //endregion
 
 
