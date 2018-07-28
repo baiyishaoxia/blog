f41c9ab3bdc5081f15053f925cff39fec88d9ca2
tangzhifu <tangzhifu@ruiec.cn>
2018-07-28 21:22:50 +0800

    Admin Model
    后台Admin Model

--- app/Http/Model/Admin/ToolsList.php
+++ app/Http/Model/Admin/ToolsList.php
@@ -4,13 +4,16 @@ namespace App\Http\Model\Admin;
 
 use App\Common\Tree;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class ToolsList extends Model
 {
+    use SoftDeletes;
     protected $table = 'tools_list';
     protected $primaryKey = 'id';
     public $timestamps = true;
     protected $guarded = [];
+    protected $dates = ['deleted_at']; //开启软删除
 
     public static function tree($type=1){
         $data = self::orderBy('sort','asc')->get()->toArray();
@@ -18,4 +21,5 @@ class ToolsList extends Model
         $tree = Tree::trees($data,'parent_id','id','text',$type);
         return $tree;
     }
+
 }
