93663bf2d7d88cfb72b5a7e0077490399517b104
tangzhifu <tangzhifu@ruiec.cn>
2018-08-03 23:44:28 +0800

    app
    A kernel file containing HTTP middleware and routing middleware

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -61,5 +61,6 @@ class Kernel extends HttpKernel
         'signed' => \Illuminate\Routing\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'admin.login'=> \App\Http\Middleware\AdminLogin::class,
+        'mobile.login'=> \App\Http\Middleware\MobileLogin::class,
     ];
 }
